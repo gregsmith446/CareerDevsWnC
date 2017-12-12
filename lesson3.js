8175735e89439778b31993bed181b1be34fcb381
gregsmith446 <gregsmith446@gmail.com>
2017-12-12 17:49:27 +0000

    started 3rd lesson

---
+++ lesson3.js
@@ -0,0 +1,13 @@
+var myComputer = {
+    operatingSystem: "mac",
+    screenSize: "15 inches",
+    purchaseYear: 2011
+};
+
+//object
+var Greg = {
+    name: "Greg",
+    sayName: function() {
+        console.log(this); //will print out the object
+    }
+};
