7ffebd9ab9707c8d630e2d93d5ecb7ea51e5f055
gregsmith446 <gregsmith446@gmail.com>
2017-12-16 21:27:12 +0000

    finished lesson 3 objects

--- lesson3.js
+++ lesson3.js
@@ -1,13 +1,42 @@
-var myComputer = {
-    operatingSystem: "mac",
-    screenSize: "15 inches",
-    purchaseYear: 2011
-};
+// var myComputer = { // example of an object that stores information
+//     operatingSystem: "mac",
+//     screenSize: "15 inches",
+//     purchaseYear: 2011
+// };
 
-//object
-var Greg = {
-    name: "Greg",
-    sayName: function() {
-        console.log(this); //will print out the object
-    }
+// var Greg = { //example of an object that stores info plus methods aka functions
+//     name: "Greg",
+//     sayName: function() { //method - a property (sayName) is equal to a function
+//         // console.log(this); //will print out the whole object
+//         console.log(this.name);
+//     }
+// };
+
+//GOALS OF VERSION 3 - OBJECTS
+// STORES the todos array on an object
+// has a DISPLAY todos method
+// has an ADD todos method
+// has a CHANGE todos method
+// has a DELETE todos method
+
+
+var todolist = { //a variable with an array inside = an object
+    todos: ["item 1", "item 2", "item 3"], //array
+    displayTodos: function() { //standalone function is a method. They don't need to be named aka "nameless" b/c we refer to it by the property name
+        //propertyname "displayTodos"
+        console.log("My Todos", "this.todos"); //displays the array in console.
+    },
+    addTodo: function(todo) { //ADD method
+        this.todos.push(todo);
+        this.displayTodos();
+    },
+    changeTodo: function(position, newValue) { //CHANGE method 
+        this.todos[position] = newValue;
+        this.displayTodos();
+    },
+    deleteTodo: function(position) { //DELETE method
+        this.todos.splice(position, 1); //delte item by index number (0, 1, 2) and only delete 1
+        this.displayTodos();
+    },
+
 };
