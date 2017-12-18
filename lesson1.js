118224dc2bf99240b7182c67f272a31b263261ca
gregsmith446 <gregsmith446@gmail.com>
2017-12-18 15:39:55 +0000

    refactoring the code

---
+++ lesson1.js
@@ -0,0 +1,26 @@
+var todos = ["item 1", "item 2", "item 3"];
+
+// console.log("hello there");
+
+// console.log(todos);
+
+// console.log('My Todos:', todos);
+
+todos.push('item 4');
+todos.push('item 5');
+
+// console.log(todos);
+
+// console.log(todos[0]);
+
+todos[0] = 'item 1 updated';
+
+// console.log(todos);
+
+todos.splice(0, 1);
+
+todos.splice(3, 1);
+
+todos.splice(3, 1);
+
+console.log(todos);
